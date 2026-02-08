d45987fec9897ba9d0b06c7f660bbfde6c284e8c
Prathmeshx17 <prathmeshalkute0@gmail.com>
2026-02-09 01:00:11 +0530

    fix cors for production

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 ======================= */
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
